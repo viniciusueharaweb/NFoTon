9c745baf80afa66430cb24f3e534eccdbcca4a3c
Breno Sapucaia <brenoss.magalhaes@outlook.com>
2021-06-08 18:20:27 -0300

    fix: adjusting folders

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
 import Hello from "../components/Hello";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <Hello />
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-);
+const IndexPage = () => <Hello />;
 
 export default IndexPage;
