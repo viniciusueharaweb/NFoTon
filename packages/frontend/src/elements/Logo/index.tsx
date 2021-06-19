805eb2c713adb0103cdf366e77fbdc95202c9373
CarmichaelF <carmichaelferreira06@gmail.com>
2021-06-19 19:37:03 -0300

    feat: adds default value to the Logo Component props

--- packages/frontend/src/elements/Logo/index.tsx
+++ packages/frontend/src/elements/Logo/index.tsx
@@ -2,13 +2,18 @@ import { Container, LogoName } from './styles'
 
 import Image from 'next/image'
 interface LogoProps {
-  imgPath: string
-  imgAlt: string
+  imgPath?: string
+  imgAlt?: string
   name?: string
   isVertical?: boolean
 }
 
-export default function Logo({ imgPath, imgAlt, name, isVertical = false }: LogoProps) {
+export default function Logo({
+  imgPath = '/logo.png',
+  imgAlt = 'Logo alt',
+  name = 'Logo name',
+  isVertical = false,
+}: LogoProps) {
   return (
     <Container isVertical={isVertical}>
       <Image width={32} height={32} src={imgPath} alt={imgAlt} />
