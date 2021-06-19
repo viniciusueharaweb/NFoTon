7d706fa7f265824c2bd2f866c50cff71647fc0ed
CarmichaelF <carmichaelferreira06@gmail.com>
2021-06-19 09:20:35 -0300

    feat: adding destructure to the Hello Component

--- packages/frontend/src/components/Hello/styles.ts
+++ packages/frontend/src/components/Hello/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  background-color: ${(props) => props.theme.backgroundColor};
-  color: ${(props) => props.theme.typography.color};
+  background-color: ${({ theme }) => theme.backgroundColor};
+  color: ${({ theme }) => theme.typography.color};
 `
